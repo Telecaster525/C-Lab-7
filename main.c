c672f5205c9f1505a6d828bdafef515d4db26d1d
Said Gadzhiev <94011547+Telecaster525@users.noreply.github.com>
2022-09-29 21:58:50 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+enum Music {
+    Classic, Pop, Rock, Rap, Punk, Indie, Metal, Folk
+};
+struct Straight {
+    int length;
+};
+union Modem {
+    int value;
+    struct {
+        unsigned char DSL: 1;
+        unsigned char PPP: 1;
+        unsigned char Link: 1;
+    };
+};
+int main(){
+    printf("#Task 1#\n");
+    enum Music m = Rock;
+    printf("%d\n", m);
+    printf("#Task 2#\n");
+    struct Straight line;
+    printf("Enter straight length:");
+    scanf("%d", &line.length);
+    printf("Output:%d\n", line.length);
+    printf("#Task 3#\n");
+    union Modem o;
+    printf("Enter the number in HEX: ");
+    scanf("%x", &o.value);
+    printf("DSL: %s || PPP: %s || Link: %s \n",
+           o.DSL ? "ON" : "OFF",
+           o.PPP ? "ON" : "OFF",
+           o.Link ? "ON" : "OFF");
+    return 0;
+}
